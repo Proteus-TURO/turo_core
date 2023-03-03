648d234f0ab15dc95a7fbf04c43621832e3499fc
Marcel Walder <marcelwalder12@gmail.com>
2023-03-03 20:10:16 +0100

    Added a setVelocity method
    Signed-off-by: Marcel Walder <marcelwalder12@gmail.com>

--- lib/turo_core.dart
+++ lib/turo_core.dart
@@ -6,10 +6,13 @@ import 'package:turo_core/exceptions/data_type_exception.dart';
 class RosBridge {
   late Ros _ros;
   late Topic _serialLed;
+  late Topic _serialDrive;
 
   RosBridge(String ipAddress, int port) {
     _ros = Ros(url: 'ws://$ipAddress:$port');
     _serialLed = Topic(ros: _ros, name: '/serial/led', type: 'std_msgs/UInt8');
+    _serialDrive =
+        Topic(ros: _ros, name: '/serial/drive', type: 'geometry_msgs/Twist');
     _ros.connect();
   }
 
@@ -19,5 +22,12 @@ class RosBridge {
     }
     Map<String, dynamic> json = {'data': brightness};
     await _serialLed.publish(json);
+  }
+
+  Future<void> setVelocity(double x, double y, double z) async {
+    Map<String, dynamic> linear = {'x': x, 'y': y, 'z': 0.0};
+    Map<String, dynamic> angular = {'x': 0.0, 'y': 0.0, 'z': z};
+    Map<String, dynamic> twist = {'linear': linear, 'angular': angular};
+    await _serialDrive.publish(twist);
   }
 }
